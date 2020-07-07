8d222077ad57e939b960764db8389f172feaf1d3
Nelson <nlparra.dev@gmail.com>
2020-07-07 23:35:27 +0000

    worked on the backend getting errors

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db,ToDo
-
+#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -37,14 +37,67 @@ def handle_todo():
         if body is None:
             raise APIException("You need to specify the request body as a json object",status_code = 400)
         if "label" not in body:
-            raise APIException("You need to specify to do ", status_code = 400)
-
-    return jsonify(response_body), 200
-todo1 = ToDo(labe=body["label"])
+            raise APIException("You need to specify a todo", status_code = 400)
+       
+        
+        todo1 = ToDo(label=body["label"],done=body["false"])
         db.session.add(todo1)
         db.session.commit()
 
         return "Ok", 200
+    
+    #Get Request
+    if request.method == "GET":
+        
+        all_todo = ToDo.query.all()
+        all_todo = list(map(lambda x : x.serialize(), all_todo))
+        return jsonify(all_todo), 200
+
+    return "Invalid Method", 404
+
+ 
+@app.route('/todo/<int:todo_id>', methods=['PUT', 'GET','DELETE'])
+def single_contact(todo_id):
+
+    #PUT Request
+    if request.method == "PUT":
+
+        body = request.get_json()
+        
+        if body is None:
+            raise APIException("You need to specify the request body as a json object",status_code = 400)
+        todo1 = ToDo.query.get(todo_id)
+        if todo1 is None:
+            raise APIException("to do not found!",404)
+        
+        if "label" in body:
+            todo1.name = body["label"]
+        
+        
+        db.session.commit()
+
+        return jsonify(todo1.serialize()),200 
+
+    #GET method
+    if request.method == "GET":
+        contact1 = ToDo.query.get(todo_id)
+        if todo1 is None:
+            raise APIException("Label not found!", status_code = 404)
+
+        return jsonify(todo1.serialize()),200
+        
+    #DELETE method
+    if request.method == "DELETE":
+        todo1 = ToDo.query.get(todo_id)
+        if todo1 is None:
+            raise APIException("To do  not found!", status_code = 404)
+
+        db.session.delete(todo1)
+        db.session.commit()
+
+        return "Ok",200
+        
+    return "Invalid method", 404
 
 
 
